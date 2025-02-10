f86fdda66f7c78a9839bf4022c759616ad902151
KoskenKolistaja <touko2000@gmail.com>
2025-02-10 22:51:57 +0200

    cctv

--- Entities/crttv_scene.gd
+++ Entities/crttv_scene.gd
@@ -5,23 +5,32 @@ extends Node3D
 
 @onready var material = $CRTTV2.get_surface_override_material(1)
 
-#@onready var screen = $CRTTV2/surface_material_override/1/albedo_texture
+@export var camera: Node3D
+
 
 var current_time = 0
 
 func _ready():
 	sub_viewport.render_target_update_mode = SubViewport.UPDATE_ONCE  # Updates only once per call
+	
+	if camera:
+		spawn_camera()
+	
+
+func spawn_camera():
+	var camera_instance = Camera3D.new()
+	camera_instance.global_transform = camera.global_transform
+	sub_viewport.add_child(camera_instance)
+
 
 
 func _physics_process(delta):
-	
-	if current_time + 500 < Time.get_ticks_msec():
-		sub_viewport.render_target_update_mode = SubViewport.UPDATE_ONCE  # Updates only once per call
-		print(Time.get_ticks_msec())
-		# Retrieve the captured Image using get_image().
-		var img = sub_viewport.get_viewport().get_texture().get_image()
-		# Convert Image to ImageTexture.
-		var tex = sub_viewport.get_texture()
-		# Set sprite texture.
-		material.albedo_texture = tex
-		current_time = Time.get_ticks_msec()
+	if camera:
+		if current_time + 100 < Time.get_ticks_msec():
+			sub_viewport.render_target_update_mode = SubViewport.UPDATE_ONCE  # Updates only once per call
+			var tex = sub_viewport.get_texture()
+			# Set sprite texture.
+			material.set_shader_parameter("tex_frg_2", tex)
+			current_time = Time.get_ticks_msec()
+	else:
+		return
