cca61cef1b21e6923f69016bdc937889ad165905
KoskenKolistaja <touko2000@gmail.com>
2025-02-10 20:21:24 +0200

    Low fps crtv

--- Entities/crttv_scene.gd
+++ Entities/crttv_scene.gd
@@ -9,13 +9,19 @@ extends Node3D
 
 var current_time = 0
 
+func _ready():
+	sub_viewport.render_target_update_mode = SubViewport.UPDATE_ONCE  # Updates only once per call
+
 
 func _physics_process(delta):
 	
-	if current_time + 200 < Time.get_ticks_msec():
+	if current_time + 500 < Time.get_ticks_msec():
+		sub_viewport.render_target_update_mode = SubViewport.UPDATE_ONCE  # Updates only once per call
+		print(Time.get_ticks_msec())
 		# Retrieve the captured Image using get_image().
 		var img = sub_viewport.get_viewport().get_texture().get_image()
 		# Convert Image to ImageTexture.
 		var tex = sub_viewport.get_texture()
 		# Set sprite texture.
 		material.albedo_texture = tex
+		current_time = Time.get_ticks_msec()
