5a19d44ca57cf12a80e03905890d81273efd7521
Leo Peltola <lopelt@utu.fi>
2025-02-22 20:05:44 +0200

    detection pretty

--- menus/detection_progress.gd
+++ menus/detection_progress.gd
@@ -3,3 +3,4 @@ extends Control
 
 func set_detection_level(level: float) -> void:
 	$ProgressBar.value = level
+	$Sprite2D.modulate = Color(1., 1., 1., ease(level, 0.6))
--- menus/hud.gd
+++ menus/hud.gd
@@ -5,10 +5,12 @@ class_name HUD
 class DetectEntry:
 	var node: DetectionProgress
 	var last_time_updated: int
+	var last_detection_level: float
 	
-	func _init(_node: DetectionProgress, time_ms: int) -> void:
+	func _init(_node: DetectionProgress, time_ms: int, _last_detection_level: float) -> void:
 		node = _node
 		last_time_updated = time_ms
+		last_detection_level = _last_detection_level
 
 
 const DetectionProgressScene := preload("res://menus/detection_progress.tscn")
@@ -19,7 +21,11 @@ var _enemy_detection_displays: Dictionary[Node3D, DetectEntry] = {}
 
 func _process(delta: float) -> void:
 	for enemy in _enemy_detection_displays:
-		if Time.get_ticks_msec() - _enemy_detection_displays[enemy].last_time_updated > 250:
+		var time_prog := (Time.get_ticks_msec() - _enemy_detection_displays[enemy].last_time_updated) / 1000.0
+		_enemy_detection_displays[enemy].node.set_detection_level(
+			lerpf(_enemy_detection_displays[enemy].last_detection_level, 0, time_prog)
+		)
+		if Time.get_ticks_msec() - _enemy_detection_displays[enemy].last_time_updated > 1000:
 			_enemy_detection_displays[enemy].node.queue_free()
 			_enemy_detection_displays.erase(enemy)
 
@@ -28,7 +34,7 @@ func set_detection_level(enemy: Node3D, detection_level: float) -> void:
 	if not _enemy_detection_displays.has(enemy):
 		var scn := DetectionProgressScene.instantiate()
 		$ScreenCenter.add_child(scn)
-		_enemy_detection_displays[enemy] = DetectEntry.new(scn, Time.get_ticks_msec())
+		_enemy_detection_displays[enemy] = DetectEntry.new(scn, Time.get_ticks_msec(), detection_level)
 	var display: DetectionProgress = _enemy_detection_displays[enemy].node
 	
 	# compute angle
@@ -40,3 +46,4 @@ func set_detection_level(enemy: Node3D, detection_level: float) -> void:
 	display.rotation = (enem_pos - p_pos).rotated(p.rotation.y).angle() + PI / 2.0
 	display.set_detection_level(detection_level)
 	_enemy_detection_displays[enemy].last_time_updated = Time.get_ticks_msec()
+	_enemy_detection_displays[enemy].last_detection_level = detection_level
