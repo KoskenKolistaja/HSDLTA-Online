df0a60ca60bdf3861b93084cadb17b9aaae7cd1e
Leo Peltola <lopelt@utu.fi>
2025-02-22 20:20:49 +0200

    detector marker scaling progress

--- menus/detection_progress.gd
+++ menus/detection_progress.gd
@@ -3,4 +3,5 @@ extends Control
 
 func set_detection_level(level: float) -> void:
 	$ProgressBar.value = level
-	$Sprite2D.modulate = Color(1., 1., 1., ease(level, 0.6))
+	$Sprite2D.modulate = Color(1., 1., 1., max(level, 0.5))
+	$Sprite2D/ColorRect.scale.x = level
